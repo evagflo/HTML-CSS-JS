d02e0c096d857aaa491acc219ca317f11b8f1524
evagflo <evagonzalezflo@gmail.com>
2021-03-25 15:18:53 +0100

    lecture 55

--- Module5/js/script.js
+++ Module5/js/script.js
@@ -1,9 +1,33 @@
+document.addEventListener("DOMContentLoaded",
+function (event){
+//1
+document.querySelector("button").addEventListener("click",sayHello);
+//document.querySelector("button").onclick = sayHello;
+//2
+document.querySelector("body").addEventListener("mousemove",
+function(event){
+    if (event.shiftKey === true){
+        console.log("x is : " + event.clientX);
+        console.log("y is : " +event.clientY);
+    }
+    
+
+}
+);
+
+})
+ 
 // DOM MANIPULATION
 //console.log(document.getElementById("title"));
 //var nom = document.getElementById("name");
 //console.log(document instanceof HTMLDocument);
 
-function sayHello(){
+
+//unobstrucing event binding --onclick="sayHello();"
+function sayHello(event){
+    console.log(event);
+    this.textContent= "Said it!";
+    
     var userName = document.getElementById("name").value;
     var message = "<h2>Hello " + userName + "!</h2>";
 
@@ -19,3 +43,4 @@ function sayHello(){
 
     }
 }
+
